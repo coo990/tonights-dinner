81606d822bb3db387dddafa812906d6c34ba865d
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-10-12 14:17:30 +0100

    feat: change database from sqlite3 to psql

--- Gemfile
+++ Gemfile
@@ -32,8 +32,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 
 group :development, :test do
   gem 'rspec-rails', '~> 5.0.0'
-  gem 'sqlite3', '~> 1.4'
-  gem 'pg', '~> 0.18.4'
+  # gem 'sqlite3', '~> 1.4'
+  gem 'pg'
 end
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_10_06_095415) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "foods", force: :cascade do |t|
     t.string "title"
     t.text "body"
