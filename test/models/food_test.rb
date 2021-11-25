ff7b88d47b6f65c3ed659bed56c5cced1e38be87
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-11-25 15:10:32 +0000

    test: implement model test

--- test/models/food_test.rb
+++ test/models/food_test.rb
@@ -3,17 +3,17 @@
 require 'test_helper'
 
 class FoodTest < ActiveSupport::TestCase
-  test "the truth" do
+  test 'the truth' do
     assert true
   end
 
-  test "should not save food without title" do
+  test 'should not save food without title' do
     food = Food.new
-    food.body = "Delicious!"
-    assert_not food.save, "Saved food without a title"
+    food.body = 'Delicious!'
+    assert_not food.save, 'Saved food without a title'
   end
 
-  test "should report error" do
+  test 'should report error' do
     assert_raises(NameError) do
       some_undefined_variable
     end
