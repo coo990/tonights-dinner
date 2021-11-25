4fe4b85e18e8dd532ccec5634b5077f007a3790d
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-11-25 11:57:15 +0000

    test: add food not save assertion

--- test/models/food_test.rb
+++ test/models/food_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class FoodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save food without title" do
+    food = Food.new
+    assert_not food.save
+  end
 end
