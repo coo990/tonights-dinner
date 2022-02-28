2eb68e0b9d158231437b4b1969e7b416ae2ae712
Chuka Okoye <chukanorth@hotmail.co.uk>
2022-02-28 10:13:10 +0000

    add: add password_digest to user column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :foods
-  before_save {self.email = email.downcase}
   
   validates :username, presence: true, uniqueness: true,
-    length: { minimum: 3, maximum: 25 }
+  length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, 
-    format: { with: VALID_EMAIL_REGEX }
+  format: { with: VALID_EMAIL_REGEX }
+
+  before_save {self.email = email.downcase}
 end
